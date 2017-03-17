d556f7b88ae475adece4cd38d53b3ba40da99d94
Urena <urenamar@amazon.com>
2017-03-17 15:23:28 -0700

    Adding npm path module

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
 app.listen(8080);
