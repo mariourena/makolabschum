69d512bc7092200c1c8218730d4a900eefe16ea7
Urena <urenamar@amazon.com>
2017-03-17 15:06:27 -0700

    Adding express wrapper

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  APIKey: 'AIzaSyCCyxxGlJixmczFksNeFfGdSQOa0E7ebYw'
+  , gcmAPIKey: 'AIzaSyA4PZh4gbcPna9OAylmnRSCx-OzWxaIeFQ'
+};
