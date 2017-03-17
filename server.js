8626c42815030e603fb5c445dbe660b2066db286
Urena <urenamar@amazon.com>
2017-03-17 15:41:01 -0700

    Trying new port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = 3000;
 
 app.use(express.static('public'));
 
@@ -9,4 +10,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
-app.listen(3000);
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!')
+})
